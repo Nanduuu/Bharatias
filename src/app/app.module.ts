d1efe6429f7f57a506002912e2e668fe47687d9b
Nanduuu <Nanduuu@users.noreply.github.com>
2019-11-23 03:09:50 +0000

    today

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    HeaderComponent,
+    TopMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
